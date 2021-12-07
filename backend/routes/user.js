671fb0d3c36e5f47a90e4a199367ba5a83ef434b
poiodecolores <valenmaldonado099@gmail.com>
2021-12-06 22:50:15 -0500

    update

---
+++ backend/routes/user.js
@@ -0,0 +1,19 @@
+import express from "express";
+import user from "../controllers/user.js";
+import auth from "../middlewares/auth.js";
+import admin from "../middlewares/admin.js";
+import validId from "../middlewares/validId.js";
+const router = express.Router();
+
+router.post("/registerUser", user.registerUser);
+router.post("/registerAdminUser", auth, admin, user.registerAdminUser);
+router.post("/login", user.login);
+router.get("/listUsers/:name?", auth, admin, user.listAllUser);
+router.get("/getRole/:email", auth, user.getUserRole);
+router.get("/findUser/:_id", auth, validId, admin, user.findUser);
+router.get("/findUserNew", auth, user.findUserNew);
+router.put("/updateUser", auth, admin, user.updateUser);
+router.put("/updateUserNew", auth, user.updateUserNew);
+router.put("/deleteUser", auth, admin, user.deleteUser);
+
+export default router;
